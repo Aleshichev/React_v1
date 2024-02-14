faffdff328e65434486bb0b87480ad2c6fbb1e5c
Igor <Aleshichev.git@email.ua>
2024-02-14 15:56:19 +0200

    added useState'

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,27 +1,60 @@
-import Button from "./Button/Button"
-import Modal from "./Modal/Modal"
-import { useState } from 'react'
+import Button from "./Button/Button";
+import Modal from "./Modal/Modal";
+import { useCallback, useEffect, useState } from "react";
+import useInput from "../hooks/useInput";
 
+export default function EffectSection() {
+  const input = useInput();
+  const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-export default function EffectSection(){
-    const [modal, setModal] = useState(false)
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    setUsers(users);
+    setLoading(false);
+  }, []);
 
-    function openModal(){
-        setModal(true)
-    }
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
-    return(
-        <section>
-            <h3>
-                Effects
-            </h3>
-            <Button onClick={openModal}>Open information</Button>
-        
-            <Modal open={modal}>
-                <h3>Heelo from modal</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque rerum labore tenetur harum repellendus nostrum cum exercitationem non vero. Quas dolor consequuntur voluptates laboriosam, iusto magnam. Dolorum placeat cum eveniet?</p>
-            <Button onClick={()=> setModal(false)}>Close modal</Button>
-            </Modal>
-        </section>
-    )
+  return (
+    <section>
+      <h3>Effects</h3>
+      <Button style={{ marginBottom: "1rem" }} onClick={() => setModal(true)}>
+        Open information
+      </Button>
+
+      <Modal open={modal}>
+        <h3>Heelo from modal</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque rerum
+          labore tenetur harum repellendus nostrum cum exercitationem non vero.
+          Quas dolor consequuntur voluptates laboriosam, iusto magnam. Dolorum
+          placeat cum eveniet?
+        </p>
+        <Button onClick={() => setModal(false)}>Close modal</Button>
+      </Modal>
+
+      {loading && <p>Loading...</p>}
+      {!loading && (
+        <>
+          <input type="text" className="control" {...input} />
+          <h6>{input.value}</h6>
+          <ul>
+            {users
+              .filter((user) =>
+                user.name.toLowerCase().includes(input.value.toLowerCase())
+              )
+              .map((user) => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+          </ul>
+        </>
+      )}
+    </section>
+  );
 }
